95dc95d3320a69242be7bf93c9cf377504155d72
felixtram <U315546@L76BT3G72.AD001.InfoFTPS.COM>
2016-12-28 16:35:43 -0700

    horizontal scales fixed

--- src/scripts/startup.js
+++ src/scripts/startup.js
@@ -9,7 +9,7 @@ import * as d3 from "d3";
 
      var margin = {top: 20, right: 150, bottom: 50, left: 40},
          width = 600 - margin.left - margin.right,
-         height = 500 - margin.top - margin.bottom;
+         height = 400 - margin.top - margin.bottom;
     
     
      var svg = d3.select("#stackid").append("svg")
@@ -24,10 +24,13 @@ var x = d3.scaleBand()
     .align(0.3);
 
 var y = d3.scaleLinear()
-    .rangeRound([height, 0]);
+    .rangeRound([height, 0])
+
+
+    var y2 = d3.scaleLinear()
+    .rangeRound([0, height]);
 
 var z = d3.scaleOrdinal(d3.schemeCategory20);
-    // .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
 
 var stack = d3.stack();
 
@@ -41,6 +44,7 @@ d3.csv("/scripts/stacked/segments_table2.csv", type, function(error, data) {
   //x.domain(data.map(function(d) { return d.ethnicity; }));
   y.domain([0, d3.max(data, function(d) { return d.total; })]).nice();
   z.domain(data.columns.slice(1));
+  y2.domain([0, d3.max(data, function(d) { return d.total; })]).nice();
 var g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
   g.selectAll(".serie")
@@ -51,26 +55,31 @@ var g = svg.append("g").attr("transform", "translate(" + margin.left + "," + mar
     .selectAll("rect")
     .data(function(d) { return d; })
     .enter().append("rect")
-      //.attr("x", function(d) { console.log (d); return x(d.data.ethnicity); })
-      .attr("y", function(d) { console.log(d); return y(d[1]); })
-      .attr("height", function(d) { return y(d[0]) - y(d[1]); })
-      .attr("width", x.bandwidth());
+      .attr("y", function(d) { return height - 75; })
+      .attr("x", function(d) {  console.log (d[0]); return y2(d[0]); })
+      .attr("width", function(d) { console.log(d); return y(d[0]) - y(d[1]); })
+      .attr("height", 50);
+
+  //g.append("g")
+  //    .attr("class", "axis axis--x")
+      //.attr("transform", "translate(0," + height + ")")
+ //     .call(d3.axisLeft(x));
 
   g.append("g")
-      .attr("class", "axis axis--x")
+//      .attr("class", "axis axis--y")
       .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x));
+      .call(
+        d3.axisBottom(y2)
+        .ticks(4, "%")
+        )
 
-  g.append("g")
-      .attr("class", "axis axis--y")
-      .call(d3.axisLeft(y).ticks(10, "s"))
     .append("text")
       .attr("x", 2)
-      .attr("y", y(y.ticks(10).pop()))
+      //.attr("y", y(y.ticks(10).pop()))
       .attr("dy", "0.35em")
       .attr("text-anchor", "start")
       .attr("fill", "#000")
-      .text("Population");
+ //     .text("Population");
 
 });
 
