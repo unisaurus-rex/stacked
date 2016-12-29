19c8fe8bde66e8b08358de5786e6802875f7acd3
felixtram <U315546@L76BT3G72.AD001.InfoFTPS.COM>
2016-12-29 11:14:28 -0700

    moved code to module, changed syntax

--- src/scripts/stacked/stacked.js
+++ src/scripts/stacked/stacked.js
@@ -2,23 +2,114 @@ import * as d3 from "d3";
 
 export default function stackChart(){
 
+  	var margin = {top: 30, right: 40, bottom: 50, left: 40};
+  	var width =0;// 900 - margin.left - margin.right;
+  	var height =0;// 300 - margin.top - margin.bottom;
+
+	var x = d3.scaleBand()
+	   	// .padding(0)
+	   	// .align(0)//
+	;
+	var y = d3.scaleLinear();
+	var y2 = d3.scaleLinear();
+	var z = d3.scaleOrdinal(d3.schemeCategory20);
+	// stuff to pass to config
+	var classMapFunction = function (d){
+		console.log(d);
+	  return classMap[ d.key ];
+	}
+
+	var classMap =  {"Department Store": "fill-blue", "Grocery": "fill-red",
+	"Family Clothing": "fill-gray-light", "Fast Food": "fill-orange-yellow",
+	"Pharmacies": "fill-teal", "All Others": "fill-gray-dark" };
+
+
+function chart(svg, data){
+	var ogheight = height 
+	width = width - margin.left- margin.right;
+	height = height - margin.top-margin.bottom;
+
+	x
+	.rangeRound([0,height]);
+	
+	y
+		.rangeRound([width,0])
+		.domain([0, d3.max(data, function(d) { return d.total; })]).nice()
+	;
+	
+	y2
+		.rangeRound([0,width])
+		.domain([0, d3.max(data, function(d) { return d.total; })]).nice()
+	;
+
+	var stack = d3.stack();
  
-
-
+  	z.domain(data.columns.slice(1));
   
-  function chart(container, dataArr){
-
-    console.log( "chart called");
+	var g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
    
+	var transformY = height- (height /4);
 
-  }
 
-/*
+	g.selectAll(".serie")
+	.data(stack.keys(data.columns.slice(0))(data))
+	.enter().append("g")
+	  .attr("class", "serie")
+	  .attr("transform", "translate(0," + transformY + ")")
+	  .attr("class", classMapFunction)
+	.selectAll("rect")
+	.data(function(d) { return d; })
+	.enter().append("rect")
+	  .attr("y", function(d) { return -height/2; })
+	  .attr("x", function(d) {   return y2(d[0]); })
+	  .attr("width", function(d) { return y(d[0]) - y(d[1]); })
+	  .attr("height", height/2);
+
+  	var xaxis = d3.axisRight(x)
+  		.tickSize(0)
+  		.ticks(0)
+  	;
+
+	g.append("g")
+		.attr("class", "axis axis--x")
+	  	.attr("transform", "translate(0," + 0+ ")")
+	  	.call(xaxis)
+  	;
+
+	g.append("g")
+		.attr("transform", "translate(0," + height + ")")
+		.call(
+			d3.axisBottom(y2)
+		  	.ticks(4, "%")
+		)
+	;
+}
+
   chart.width = function(value){
     if (!arguments.length) return width;
     width = value;
     return chart;
-  }*/
+  }
+    chart.margin = function(value){
+    if (!arguments.length) return margin;
+    margin = value;
+    return chart;
+  }
+    chart.height = function(value){
+    if (!arguments.length) return height;
+    height = value;
+    return chart;
+  }
+  chart.classMap = function(value){
+    if (!arguments.length) return classMap;
+    classMap = value;
+    return chart;
+  }
+  chart.classMapFunction = function(value){
+    if(!arguments.length) return classMapFunction;
+    classMapFunction = value;
+    return chart;
+  }
 
 
     return chart;
--- src/scripts/startup.js
+++ src/scripts/startup.js
@@ -4,92 +4,37 @@ import bootstrap from 'bootstrap-sass';
 import stackChart from 'stacked';
 import * as d3 from "d3";
 
+var data= [ { "All Others": 0.2,
+  "Department Store": 0.2,
+  "Family Clothing": 0.2,
+  "Fast Food": 0.2,
+  "Grocery": 0.1,
+  "Pharmacies": 0.1,
+  total: 1 } ];
+
+//add columns attribute
+data.columns = Object.keys(data[0]).filter(function (obj){
+  return obj != "total";
+})
+  
+var svg = d3.select("#stackid")  .append("div")
+  .classed("svg-container", true)
+  .append("svg")
+  .attr("preserveAspectRatio", "xMinYMin meet")     
+  .attr("viewBox","0 0 " + 900 + " " + 300)
+  //class to make it responsive
+  .classed("svg-content-responsive", true)
+;
 
 
+var margin = {top: 30, right: 40, bottom: 50, left: 40};
+var width =900;
+var height =300;
 
-     var margin = {top: 20, right: 150, bottom: 50, left: 40},
-         width = 600 - margin.left - margin.right,
-         height = 400 - margin.top - margin.bottom;
-    
-    
-     var svg = d3.select("#stackid").append("svg")
-         .attr("width", width + margin.left + margin.right)
-         .attr("height", height + margin.top + margin.bottom)
-       .append("g")
-         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+var testStack = stackChart()
+  .margin(margin)
+  .width(width)
+  .height(height)
+;
 
-var x = d3.scaleBand()
-    .rangeRound([0, width])
-    .padding(0.3)
-    .align(0.3);
-
-var y = d3.scaleLinear()
-    .rangeRound([height, 0])
-
-
-    var y2 = d3.scaleLinear()
-    .rangeRound([0, height]);
-
-var z = d3.scaleOrdinal(d3.schemeCategory20);
-
-var stack = d3.stack();
-
-d3.csv("/scripts/stacked/segments_table2.csv", type, function(error, data) {
-
-  //console.log(data)
-  if (error) throw error;
-
-  data.sort(function(a, b) { return b.total - a.total; });
-
-  //x.domain(data.map(function(d) { return d.ethnicity; }));
-  y.domain([0, d3.max(data, function(d) { return d.total; })]).nice();
-  z.domain(data.columns.slice(1));
-  y2.domain([0, d3.max(data, function(d) { return d.total; })]).nice();
-var g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-  g.selectAll(".serie")
-    .data(stack.keys(data.columns.slice(0))(data))
-    .enter().append("g")
-      .attr("class", "serie")
-      .attr("fill", function(d) { return z(d.key); })
-    .selectAll("rect")
-    .data(function(d) { return d; })
-    .enter().append("rect")
-      .attr("y", function(d) { return height - 75; })
-      .attr("x", function(d) {  console.log (d[0]); return y2(d[0]); })
-      .attr("width", function(d) { console.log(d); return y(d[0]) - y(d[1]); })
-      .attr("height", 50);
-
-  //g.append("g")
-  //    .attr("class", "axis axis--x")
-      //.attr("transform", "translate(0," + height + ")")
- //     .call(d3.axisLeft(x));
-
-  g.append("g")
-//      .attr("class", "axis axis--y")
-      .attr("transform", "translate(0," + height + ")")
-      .call(
-        d3.axisBottom(y2)
-        .ticks(4, "%")
-        )
-
-    .append("text")
-      .attr("x", 2)
-      //.attr("y", y(y.ticks(10).pop()))
-      .attr("dy", "0.35em")
-      .attr("text-anchor", "start")
-      .attr("fill", "#000")
- //     .text("Population");
-
-});
-
-function type(d, i, columns) {
-
-  //console.log("before", d)
-  var i;
-  var t;
-  for (i = 0, t = 0; i < columns.length; ++i) t += d[columns[i]] = +d[columns[i]];
-  d.total = t;
-//console.log("after", d)
-  return d;
-}
+testStack(svg, data);
